f03e436450257404b376d18a21f01d976424effd
Arun <karanam.arun77@gmail.com>
2021-09-23 10:44:54 +0530

    Add project files.

---
+++ ParkyAPI/Model/Dtos/TrailCreateDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkyAPI.Model.Trail;
+
+namespace ParkyAPI.Model.Dtos
+{
+    public class TrailCreateDto
+    {
+        
+        
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Distance { get; set; }
+        [Required]
+        public double Elevation { get; set; }
+        public DifficultyType Difficulty { get; set; }
+        [Required]
+
+        public int NationalParkId { get; set; }
+        
+
+    }
+}
---
+++ ParkyWebMVC/Repository/IRepository/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using ParkyWeb.Repository.IRepository;
+using ParkyWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWebMVC.Repository.IRepository
+{
+    public interface IAccountRepository:IRepository<User>
+    {
+        Task<User> LoginAsync(string url, User objToCreate);
+        Task<bool> RegisterAsync(string url, User objToCreate);
+
+    }
+}
